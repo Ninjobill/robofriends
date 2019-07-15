0b220fd1e390892a9bb17577f17637f16d839fc9
Billy Todd <billy.todd50@yahoo.com>
2019-07-14 17:40:15 -0700

    Updates

---
+++ precache-manifest.0edb3c18bbf87a1fcb4f9f1ccdbf363a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "654ceb7c5a4204ae08ee7454478bf74d",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "4c07447c9f4d1e9e4461",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "48294bcfbebf8b2fb5b5",
+    "url": "/robofriends/static/css/main.c6a9fc4a.chunk.css"
+  },
+  {
+    "revision": "4c07447c9f4d1e9e4461",
+    "url": "/robofriends/static/js/2.c729cc22.chunk.js"
+  },
+  {
+    "revision": "48294bcfbebf8b2fb5b5",
+    "url": "/robofriends/static/js/main.22cd2bde.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
